4797ad4fbc75e0bb9a05351fc5bc2cb8ec09dbd7
Neelov Eugene <neelov.ev@mail.ru>
2023-11-06 03:15:28 +0300

    Added bot constructor and start method

--- Bot/Bot.cs
+++ Bot/Bot.cs
@@ -1,16 +1,19 @@
-﻿namespace Bot;
+﻿using DSharpPlus;
 
-public class Bot
+namespace Bot;
+
+public class Bot(string token)
 {
-    private string Token { get; set; }
-    
-    public class Bot(string token)
-    {
-        
-    }
-    
+    private string Token { get; set; } = token;
+
     public async Task Start()
     {
+        var discord = new DiscordClient(new DiscordConfiguration
+        {
+            Token = Token,
+            TokenType = TokenType.Bot
+        });
+        
         
     }
 }
