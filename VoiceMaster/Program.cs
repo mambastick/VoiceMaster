6fbf27804901c01f8af935518a46735542ff796d
Neelov Eugene <neelov.ev@mail.ru>
2023-11-06 13:30:12 +0300

    Added test database

--- VoiceMaster/Program.cs
+++ VoiceMaster/Program.cs
@@ -13,7 +13,7 @@ class Program
         var bot = new Bot(
             "MTE3MDg3MTc1MjU2MjMxNTI2NQ.GQ4Fgm.a7wz7WzPsWOqVpY9IQXhug3mJ-pZFscB3DKNZc",
             new Logger(),
-            new MySqlDatabase("1", "1", "1", "1"));
+            new MySqlDatabase("1", "1", "1", "1")); 
         bot.StartAsync();
         await Task.Delay(-1);
     }
