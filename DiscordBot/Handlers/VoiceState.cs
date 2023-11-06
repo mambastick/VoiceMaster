a2f338adcefca72d7a0163e0a4675785fcc39287
Neelov Eugene <neelov.ev@mail.ru>
2023-11-06 04:01:00 +0300

    Added class VoiceState.cs for created voice channels

---
+++ DiscordBot/Handlers/VoiceState.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace DiscordBot.Handlers;
+
+public class VoiceState
+{
+    private static readonly string CreatedVoiceChannelName = "🔊│Создать канал";
+    public static Dictionary<ulong, ulong> VoiceChannels = new();
+
+    private async Task CreateVoiceChannelAsync(DiscordClient sender, VoiceStateUpdateEventArgs e)
+    {
+        
+    }
+
+    private async Task DeleteVoiceChannelAsync(DiscordClient sender, VoiceStateUpdateEventArgs e)
+    {
+        
+    }
+
+    public static async Task VoiceStateUpdatedAsync(DiscordClient sender, VoiceStateUpdateEventArgs e)
+    {
+        try
+        {
+            Bot.Logger.LogInformation(JsonSerializer.Serialize(e));
+        }
+        catch (Exception ex)
+        {
+            Bot.Logger.LogError(ex.Message);
+        }
+    }
+}
