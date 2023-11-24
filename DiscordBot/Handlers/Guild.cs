362027ac611e96942b294a1e72b057349e40e736
Neelov Eugene <neelov.ev@mail.ru>
2023-11-24 11:37:53 +0300

    Added Guild.cs for get info when bot join in new guild

---
+++ DiscordBot/Handlers/Guild.cs
@@ -0,0 +1,42 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+
+namespace DiscordBot.Handlers;
+
+public class Guild
+{
+    public static async Task GuildCreatedAsync(DiscordClient client, GuildCreateEventArgs createdGuild)
+    {
+        try
+        {
+            Bot.Logger.LogInformation($"Bot was added in new guild: {createdGuild.Guild.Name}");
+            
+            var defaultChannel = createdGuild.Guild.GetDefaultChannel();
+
+            var embedGettingStarted = new DiscordEmbedBuilder()
+                .WithTitle("Getting started")
+                .WithDescription("Hello, dear administrators.\n" +
+                    "I'm glad you added me to your server.\n\n" +
+                    "I was created to make temporary voice channels for users of your server.\n" +
+                    "Just use Setup button.")
+                .WithColor(DiscordColor.IndianRed);
+
+            var helloMessage = new DiscordMessageBuilder()
+                .WithEmbed(embedGettingStarted)
+                .AddComponents(new List<DiscordComponent>()
+                {
+                    new DiscordButtonComponent(ButtonStyle.Success, 
+                        "create_setup_channel",
+                        "Setup", 
+                        emoji: new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":loud_sound:")))
+                });
+            
+            await defaultChannel.SendMessageAsync(helloMessage);
+        }
+        catch (Exception ex)
+        {
+            Bot.Logger.LogError(ex.ToString());
+        }
+    }
+}
