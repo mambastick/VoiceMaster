1ef43d6aea1b381cc7447166ab5e1f42899d4195
Neelov Eugene <neelov.ev@mail.ru>
2023-11-27 20:42:38 +0300

    Added Task.Run in methods for optimization program

--- DiscordBot/Handlers/Interaction.cs
+++ DiscordBot/Handlers/Interaction.cs
@@ -11,39 +11,42 @@ public class Interaction
     {
         try
         {
-            Bot.Logger.LogInformation($"User have been pressed button {ctx.Id}");
-            switch (ctx.Id)
+            _ = Task.Run(async () =>
             {
-                case "create_setup_channel":
-                    var voiceChannel = new VoiceChannel();
-                    var setupChannelExist = await voiceChannel.GetChannelIdAsync(ctx.Guild.Id,
-                        channelType: Data.ChannelType.Setup);
-                    if (setupChannelExist is not null)
-                        throw new Exception("The setup channel has already been created.");
+                Bot.Logger.LogInformation($"User have been pressed button {ctx.Id}");
+                switch (ctx.Id)
+                {
+                    case "create_setup_channel":
+                        var voiceChannel = new VoiceChannel();
+                        var setupChannelExist = await voiceChannel.GetChannelIdAsync(ctx.Guild.Id,
+                            channelType: Data.ChannelType.Setup);
+                        if (setupChannelExist is not null)
+                            throw new Exception("The setup channel has already been created.");
 
-                    var parent = await ctx.Guild.CreateChannelCategoryAsync("Voice Master");
-                    var setupVoiceChannel = await ctx.Guild.CreateVoiceChannelAsync("Create channel", parent);
-                    await voiceChannel.AddToDatabaseAsync(setupVoiceChannel, Data.ChannelType.Setup);
-                    
-                    var embedGettingStarted = new DiscordEmbedBuilder()
-                        .WithTitle("Getting started")
-                        .WithDescription("Hello, dear administrators.\n" +
-                            "I'm glad you added me to your server.\n\n" +
-                            "I was created to make temporary voice channels for users of your server.\n" +
-                            "Just use Setup button.")
-                        .WithColor(DiscordColor.IndianRed);
+                        var parent = await ctx.Guild.CreateChannelCategoryAsync("Voice Master");
+                        var setupVoiceChannel = await ctx.Guild.CreateVoiceChannelAsync("Create channel", parent);
+                        await voiceChannel.AddToDatabaseAsync(setupVoiceChannel, Data.ChannelType.Setup);
 
-                    await ctx.Interaction.CreateResponseAsync(
-                        InteractionResponseType.UpdateMessage,
-                        new DiscordInteractionResponseBuilder()
-                            .AddEmbed(embedGettingStarted)
-                            .AddComponents(new DiscordButtonComponent(ButtonStyle.Success,
-                                "create_setup_channel",
-                                "Setup",
-                                true,
-                                new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":loud_sound:")))));
-                    return;
-            }
+                        var embedGettingStarted = new DiscordEmbedBuilder()
+                            .WithTitle("Getting started")
+                            .WithDescription("Hello, dear administrators.\n" +
+                                "I'm glad you added me to your server.\n\n" +
+                                "I was created to make temporary voice channels for users of your server.\n" +
+                                "Just use Setup button.")
+                            .WithColor(DiscordColor.IndianRed);
+
+                        await ctx.Interaction.CreateResponseAsync(
+                            InteractionResponseType.UpdateMessage,
+                            new DiscordInteractionResponseBuilder()
+                                .AddEmbed(embedGettingStarted)
+                                .AddComponents(new DiscordButtonComponent(ButtonStyle.Success,
+                                    "create_setup_channel",
+                                    "Setup",
+                                    true,
+                                    new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":loud_sound:")))));
+                        return;
+                }
+            });
         }
         catch (Exception ex) when (ex.Message == "The setup channel has already been created.")
         {
