0d630a9f0cbab0c4f8ff9b913820edeb8beab22b
mambastick <neelov.ev@mail.ru>
2024-04-08 23:48:01 +0300

    Добавлена модель создающего канала

---
+++ VoiceMaster/Models/SetupChannel.cs
@@ -0,0 +1,13 @@
+﻿namespace VoiceMaster.Models;
+
+public class SetupChannel
+{
+    // Уникальный ID сервера, на котором создан канал для создания каналов
+    public ulong GuildId { get; set; }
+    
+    // Уникальный ID канала создающего канала
+    public ulong ChannelId { get; set; }
+    
+    // Коллекция временных каналов, связанных с этим SetupChannel
+    public ICollection<TempChannel>? TempChannels { get; set; }
+}
