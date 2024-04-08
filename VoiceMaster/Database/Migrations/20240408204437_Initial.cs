13060cf91c9050de987080c92ce35342ea6c94bb
mambastick <neelov.ev@mail.ru>
2024-04-08 23:49:51 +0300

    Создана первая (инициализирующая) миграция

---
+++ VoiceMaster/Database/Migrations/20240408204437_Initial.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoiceMaster.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "SetupChannels",
+                columns: table => new
+                {
+                    GuildId = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ChannelId = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SetupChannels", x => x.GuildId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TempChannels",
+                columns: table => new
+                {
+                    ChannelId = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    GuildId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    SetupChannelId = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TempChannels", x => x.ChannelId);
+                    table.ForeignKey(
+                        name: "FK_TempChannels_SetupChannels_SetupChannelId",
+                        column: x => x.SetupChannelId,
+                        principalTable: "SetupChannels",
+                        principalColumn: "GuildId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TempChannels_SetupChannelId",
+                table: "TempChannels",
+                column: "SetupChannelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TempChannels_UserId",
+                table: "TempChannels",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TempChannels");
+
+            migrationBuilder.DropTable(
+                name: "SetupChannels");
+        }
+    }
+}
