9d6fbe85f61a1418636e98000d967d8b0a47f991
mambastick <neelov.ev@mail.ru>
2024-04-08 21:45:27 +0300

    Очищен главный класс

--- VoiceMaster/Program.cs
+++ VoiceMaster/Program.cs
@@ -1,20 +1,9 @@
-﻿
+﻿namespace VoiceMaster;
 
-using Database;
-using DiscordBot;
-using LoggerService;
-
-namespace VoiceMaster;
-
-internal abstract class Program
+public class Program
 {
     private static async Task Main()
     {
-        var bot = new Bot(
-            "MTE3MDg3MTc1MjU2MjMxNTI2NQ.GQ4Fgm.a7wz7WzPsWOqVpY9IQXhug3mJ-pZFscB3DKNZc",
-            new Logger(),
-            new MySqlDatabase("mambaskills.ru", "VoiceMaster", "VoiceMaster", "jVX-6ti-m3V-yYq")); 
-        await bot.StartAsync();
-        await Task.Delay(-1);
+        
     }
 }
