b5199b8f37766b991b55a45b1165275949debfc1
mambastick <neelov.ev@mail.ru>
2024-04-09 21:44:56 +0300

    Добавлена команда установки создавающего канала

---
+++ VoiceMaster/Commands/SlashCommands/SetupCommand.cs
@@ -0,0 +1,49 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using Serilog;
+using VoiceMaster.Models;
+
+namespace VoiceMaster.Commands.SlashCommands;
+
+public class VoiceChannelCommands : ApplicationCommandModule
+{
+    [SlashCommand("setup", "Создать голосовой канал для создания временных голосовых каналов")]
+    public async Task SetupCommandAsync(InteractionContext context)
+    {
+        try
+        {
+            var voiceChannel = await new SetupChannel().GetAsync(context.Guild.Id);
+            if (voiceChannel is not null)
+                throw new Exception("Создающий голосовой канал уже установлен.");
+
+            var parent = await context.Guild.CreateChannelCategoryAsync("Voice Master");
+            var setupVoiceChannel = await context.Guild.CreateVoiceChannelAsync("Создать канал", parent);
+
+            await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder()
+                    .WithContent("Канал для создания временных голосовых каналов был успешно создан.\n" +
+                                 $"Чтобы войти в голосовой канал, нажмите здесь: {setupVoiceChannel.Mention}\n\n" +
+                                 "Вы также можете переместить этот канал в любое удобное для вас место, а также изменить его название.")
+                    .AsEphemeral());
+
+            voiceChannel.ChannelId = setupVoiceChannel.Id;
+            voiceChannel.GuildId = context.Guild.Id;
+            await voiceChannel.AddAsync();
+        }
+        catch (Exception ex) when (ex.Message == "The setup channel has already been created.")
+        {
+            await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder()
+                    .WithContent(ex.Message)
+                    .AsEphemeral());
+        }
+        catch (Exception ex)
+        {
+            await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder()
+                    .WithContent("Произошла ошибка, канал для создания временных голосовых каналов не был создан.")
+                    .AsEphemeral());
+        }
+    }
+}
