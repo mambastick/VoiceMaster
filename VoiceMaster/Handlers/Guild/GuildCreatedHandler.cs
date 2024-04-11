6c4b3cd4dab01ffca641322903e6fb0b242df11c
mambastick <neelov.ev@mail.ru>
2024-04-12 01:44:40 +0300

    Написана функция, которая срабатывает, когда бота добавляют на новый сервер (приветствие и настройка)

--- VoiceMaster/Handlers/Guild/GuildCreatedHandler.cs
+++ VoiceMaster/Handlers/Guild/GuildCreatedHandler.cs
@@ -1,5 +1,7 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using Serilog;
 
 namespace VoiceMaster.Handlers.Guild;
 
@@ -7,6 +9,41 @@ public class GuildCreatedHandler
 {
     public Task ClientOnGuildCreated(DiscordClient sender, GuildCreateEventArgs args)
     {
+        try
+        {
+            _ = Task.Run(async () =>
+            {
+                Log.Logger.Information($"Бот был добавлен на новый сервер: {args.Guild.Name}");
+
+                var defaultChannel = args.Guild.GetDefaultChannel();
+
+                var embedGettingStarted = new DiscordEmbedBuilder()
+                    .WithTitle("Начало работы")
+                    .WithDescription("Привет, уважаемые администраторы.\n" +
+                                     "Я рад, что вы добавили меня на свой сервер.\n\n" +
+                                     "Меня создали для создания временных голосовых каналов для пользователей вашего сервера.\n" +
+                                     "Просто используйте кнопку \"Создать создающий канал\".")
+                    .WithColor(DiscordColor.IndianRed);
+
+                var helloMessage = new DiscordMessageBuilder()
+                    .WithEmbed(embedGettingStarted)
+                    .AddComponents(new List<DiscordComponent>()
+                    {
+                        new DiscordButtonComponent(ButtonStyle.Success,
+                            "create_setup_channel",
+                            "Создать создающий канал",
+                            emoji: new DiscordComponentEmoji(DiscordEmoji.FromName(sender, ":loud_sound:")))
+                    });
+
+                await defaultChannel.SendMessageAsync(helloMessage);
+            });
+        }
+        catch (Exception ex)
+        {
+            Log.Logger.Error(ex, ex.Message);
+            throw;
+        }
+        
         return Task.CompletedTask;
     }
 }
