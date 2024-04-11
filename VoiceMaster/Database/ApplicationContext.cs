af7635235cd3f289afc42c602b59f1fbde0832d6
mambastick <neelov.ev@mail.ru>
2024-04-12 00:29:08 +0300

    Исправлен класс базы данных под новый  провайдер ef core

--- VoiceMaster/Database/ApplicationContext.cs
+++ VoiceMaster/Database/ApplicationContext.cs
@@ -19,33 +19,25 @@ public class ApplicationContext : DbContext
                                $"port={env["DATABASE_PORT"]};" +
                                $"database={env["DATABASE_NAME"]};" +
                                $"user={env["DATABASE_USER"]};" +
-                               $"password={env["DATABASE_PASSWORD"]}";;
-        
-        // Вычисляем версию MySQL
-        var mySqlVersion = ServerVersion.AutoDetect(connectionString);
+                               $"password={env["DATABASE_PASSWORD"]}";
         
         // Используем базу данных MySQL
-        optionsBuilder.UseMySql(
-            connectionString: connectionString,
-            serverVersion: mySqlVersion
-            );
+        optionsBuilder.UseMySQL(
+            connectionString: connectionString);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // Настройка первичных ключей и уникальных ограничений
         modelBuilder.Entity<SetupChannel>()
-            .HasKey(s => s.GuildId); // Уникальный ID сервера как первичный ключ
-        modelBuilder.Entity<TempChannel>()
-            .HasKey(t => t.ChannelId); // Уникальный ID канала как первичный ключ
-        modelBuilder.Entity<TempChannel>()
-            .HasIndex(t => t.UserId); // Индекс для ускорения поиска по UserId
+            .HasKey(sc => sc.ChannelId);
 
-        // Настройка связи между таблицами с каскадным удалением
-        modelBuilder.Entity<SetupChannel>()
-            .HasMany(s => s.TempChannels)
-            .WithOne(t => t.SetupChannel)
-            .HasForeignKey(t => t.SetupChannelId) // Используем SetupChannelId как внешний ключ
+        modelBuilder.Entity<TempChannel>()
+            .HasKey(tc => tc.ChannelId);
+
+        modelBuilder.Entity<TempChannel>()
+            .HasOne(sc => sc.SetupChannel)
+            .WithMany(s => s.TempChannels)
+            .HasForeignKey(sc => sc.SetupChannelId)
             .OnDelete(DeleteBehavior.Cascade);
     }
 }
