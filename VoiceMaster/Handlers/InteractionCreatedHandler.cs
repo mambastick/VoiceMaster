0ea2e863238353d8a90d67a2bea4e8147fede25c
mambastick <neelov.ev@mail.ru>
2024-04-12 02:06:23 +0300

    Написан алгоритм для кнопки "Создать создающий канал", но имеется баг в заполнении данных

--- VoiceMaster/Handlers/InteractionCreatedHandler.cs
+++ VoiceMaster/Handlers/InteractionCreatedHandler.cs
@@ -1,5 +1,9 @@
-﻿using DSharpPlus;
+﻿using System.Reflection;
+using DSharpPlus;
 using DSharpPlus.EventArgs;
+using DSharpPlus.SlashCommands;
+using Serilog;
+using VoiceMaster.Commands.SlashCommands;
 
 namespace VoiceMaster.Handlers;
 
@@ -7,6 +11,42 @@ public class InteractionCreatedHandler
 {
     public Task ClientOnComponentInteractionCreated(DiscordClient sender, ComponentInteractionCreateEventArgs args)
     {
+        try
+        {
+            _ = Task.Run(async () =>
+            {
+                Log.Logger.Information($"User have been pressed button {args.Id}");
+                switch (args.Id)
+                {
+                    case "create_setup_channel":
+                        var interactionContext = new InteractionContext();
+
+                        var interactionProperties = interactionContext.GetType()
+                            .GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                            .Where(prop => prop.CanWrite && prop.DeclaringType == typeof(InteractionContext));
+
+                        foreach (var prop in interactionProperties)
+                        {
+                            // TODO: Пофиксить баг с заполнением данных по полям класса
+                            var argProp = args.GetType().GetProperty(prop.Name);
+                            if (argProp == null || argProp.PropertyType != prop.PropertyType) continue;
+                            var value = argProp.GetValue(args);
+                            prop.SetValue(interactionContext, value);
+                        }
+
+                        await new SetupCommand().SetupCommandAsync(interactionContext);
+                        return Task.CompletedTask;
+                }
+
+                return Task.CompletedTask;
+            });
+        }
+        catch (Exception ex)
+        {
+            Log.Logger.Error(ex, ex.Message);
+            throw;
+        }
+
         return Task.CompletedTask;
     }
 }
