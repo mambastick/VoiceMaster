d251c244c9d70b6d2414a9ee0e41c3b4f61b9eac
mambastick <neelov.ev@mail.ru>
2024-04-13 18:45:25 +0300

    Рефакторинг

--- VoiceMaster/Program.cs
+++ VoiceMaster/Program.cs
@@ -7,36 +7,36 @@ using Serilog.Sinks.Discord;
 using Serilog.Sinks.SystemConsole.Themes;
 using VoiceMaster.Database;
 
-namespace VoiceMaster;
-
-public class Program
+namespace VoiceMaster
 {
-    private static async Task Main()
+    public class Program
     {
-        // Загрузка переменных сред
-        var env = AppSettings.GetAppSettings();
+        private static async Task Main()
+        {
+            // Загрузка переменных сред
+            var env = AppSettings.GetAppSettings();
 
-        // Создание логгера
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.Console(
-                theme: SystemConsoleTheme.Colored,
-                restrictedToMinimumLevel: LogEventLevel.Information,
-                outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss} [{Level:u}] {Message:lj}{NewLine}{Exception}"
-            ) // Логируем в косноль
-            .WriteTo.File(
-                path: $"logs/{DateTime.Now:dd-MM-yyyy_HH-mm-ss}.log",
-                outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss} [{Level:u}] {Message:lj}{NewLine}{Exception}",
-                restrictedToMinimumLevel: LogEventLevel.Information
-            ) // Логируем в файл
-            .WriteTo.Async(a =>
-            {
-                a.Discord(
-                    webhookId: Convert.ToUInt64(env["WEBHOOK_ID"]),
-                    webhookToken: env["WEBHOOK_URL"],
+            // Создание логгера
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(
+                    theme: SystemConsoleTheme.Colored,
+                    restrictedToMinimumLevel: LogEventLevel.Information,
+                    outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss} [{Level:u}] {Message:lj}{NewLine}{Exception}"
+                ) // Логируем в консоль
+                .WriteTo.File(
+                    path: $"logs/{DateTime.Now:dd-MM-yyyy_HH-mm-ss}.log",
+                    outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss} [{Level:u}] {Message:lj}{NewLine}{Exception}",
                     restrictedToMinimumLevel: LogEventLevel.Information
-                ); // Логируем в Discord
-            })
-            .CreateLogger();
+                ) // Логируем в файл
+                .WriteTo.Async(a =>
+                {
+                    a.Discord(
+                        webhookId: Convert.ToUInt64(env["WEBHOOK_ID"]),
+                        webhookToken: Convert.ToString(env["WEBHOOK_URL"]),
+                        restrictedToMinimumLevel: LogEventLevel.Information
+                    ); // Логируем в Discord
+                })
+                .CreateLogger();
 
             // Политика ретрая для ожидания доступности базы данных
             var retryPolicy = Policy
@@ -61,23 +61,24 @@ public class Program
             // Создаем бота
             var voiceMaster = new Bot(token: Convert.ToString(env["BOT_TOKEN"]));
 
-        try
-        {
-            // Запускаем бота
-            await voiceMaster.StartAsync();
-        }
-        catch (Exception ex)
-        {
-            Log.Logger.Error(ex, ex.Message);
-            throw;
-        }
-        finally
-        {
-            // Останавливаем бота
-            await voiceMaster.StopAsync();
+            try
+            {
+                // Запускаем бота
+                await voiceMaster.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex, ex.Message);
+                throw;
+            }
+            finally
+            {
+                // Останавливаем бота
+                await voiceMaster.StopAsync();
 
-            // Закрываем логгер и очищаем память
-            await Log.CloseAndFlushAsync();
+                // Закрываем логгер и очищаем память
+                await Log.CloseAndFlushAsync();
+            }
         }
     }
 }
