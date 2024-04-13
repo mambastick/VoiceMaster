d2963a216a6826731a066ed80a7d328b523cce8d
mambastick <neelov.ev@mail.ru>
2024-04-13 18:42:38 +0300

    Удалена библиотека dotenv.net, так как она не видела env в Docker

--- VoiceMaster/AppSettings.cs
+++ VoiceMaster/AppSettings.cs
@@ -1,13 +1,13 @@
-﻿using dotenv.net;
+﻿using System.Collections;
 
 namespace VoiceMaster;
 
 public abstract class AppSettings
 {
-    public static IDictionary<string, string> GetAppSettings()
+    public static IDictionary GetAppSettings()
     {
-        // Загрузка переменных сред
-        var env = DotEnv.Read();
+        // Получаем все переменные среды
+        var env = Environment.GetEnvironmentVariables();
         
         // Проверка переменных сред
         ValidateEnvironment(env);
@@ -15,15 +15,11 @@ public abstract class AppSettings
         return env;
     }
 
-    private static void ValidateEnvironment(IDictionary<string, string> env)
+    private static void ValidateEnvironment(IDictionary env)
     {
-        foreach (var key in env.Keys)
-        {
-            var value = env[key];
-            if (string.IsNullOrEmpty(value))
-            {
-                throw new Exception($"{key} не может быть пустым!");
-            }
-        }
+        // Проверяем наличие ожидаемых переменных среды
+        var expectedVariables = new List<string> { "BOT_TOKEN", "WEBHOOK_ID", "WEBHOOK_URL", "DATABASE_HOST", "DATABASE_PORT", "DATABASE_NAME", "DATABASE_USER", "DATABASE_PASSWORD" };
+        foreach (var key in expectedVariables.Where(key => !env.Contains(key)))
+            throw new Exception($"Отсутствует переменная среды {key}!");
     }
 }
