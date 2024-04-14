60f54ae8f7931df1cf117cc7cc671b524abe5fa1
mambastick <neelov.ev@mail.ru>
2024-04-14 13:55:03 +0300

    Добавлена обработка файла .env , если переменные среды не указаны

--- VoiceMaster/AppSettings.cs
+++ VoiceMaster/AppSettings.cs
@@ -6,8 +6,31 @@ public abstract class AppSettings
 {
     public static IDictionary GetAppSettings()
     {
-        // Получаем все переменные среды
-        var env = Environment.GetEnvironmentVariables();
+        var env = new Dictionary<string, string>();
+
+        // Проверяем наличие файла .env и считываем переменные, если файл существует
+        if (File.Exists(".env"))
+        {
+            var lines = File.ReadAllLines(".env");
+            foreach (var line in lines)
+            {
+                var parts = line.Split('=', 2);
+                if (parts.Length == 2)
+                {
+                    var key = parts[0].Trim();
+                    var value = parts[1].Trim();
+                    env[key] = value;
+                }
+            }
+        }
+        else // Если файла .env нет, читаем переменные из среды системы
+        {
+            var environmentVariables = Environment.GetEnvironmentVariables();
+            foreach (var key in environmentVariables.Keys)
+            {
+                env[key.ToString()] = environmentVariables[key].ToString();
+            }
+        }
         
         // Проверка переменных сред
         ValidateEnvironment(env);
